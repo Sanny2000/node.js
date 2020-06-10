d16da1d999aee9e4ba7b424e1b8f10dba4413ad1
liuailing <liuailiu@3223.com>
2020-06-10 23:33:25 +0800

    1.createServer 2.response.writeHead 3.fs.readFileSync 4.fs.readFile 5.eventEmitter 6.Node.js 事件循环

---
+++ 程序/Node.js的优势/service/user.js
@@ -0,0 +1,17 @@
+module.exports = {
+    getUser() {
+        return [{
+                name: "A"
+            },
+            {
+                name: "B"
+            },
+            {
+                name: "C"
+            },
+            {
+                name: "D"
+            }
+        ];
+    }
+};
